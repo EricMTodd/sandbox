614da9cf860fef9c868c7d0a91501e5dc73bc5b2
EricMTodd <eterictodd@gmail.com>
2019-12-19 08:50:43 -0700

    cosider using a while loop to fix current bugs of list being deleted entirely, or elements not being deleted at all.

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,8 +1,6 @@
-# import os
 from random import randint
 
 n = randint(1, 100)
-# print(n)
 numbers_list = []
 
 for i in range(1, 101):
@@ -13,19 +11,16 @@ print(numbers_list)
 def guess_engine():
     guess = int(input("Please enter a number between 1 and 100: "))
     if guess < n:
-        # os.system('cls' if os.name == 'nt' else 'clear')
         print(f"Please guess a higher number than {guess}.")
         del numbers_list[:guess]
         print(numbers_list)
         guess_engine()
     elif guess > n:
-        # os.system('cls' if os.name == 'nt' else 'clear')
         print(f"Please guess a lower number than {guess}.")
         del numbers_list[guess - 1:]
         print(numbers_list)
         guess_engine()
     else:
-        # os.system('cls' if os.name == 'nt' else 'clear')
         print("You guessed the correct number!")
 
 
