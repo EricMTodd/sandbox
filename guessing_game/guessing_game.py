f96930e5e31f913525466966b9cadf75d41d4fce
EricMTodd <eterictodd@gmail.com>
2019-12-19 13:04:34 -0700

    working on fixing bug. The solution lies in matching an index value, whereas I was trying to use the value of the guess as the index.

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,24 +1,17 @@
 from random import randint
 
 n = randint(1, 100)
-numbers_list = []
-
-for i in range(1, 101):
-    numbers_list.append(i)
-print(numbers_list)
+l = list(range(1, 101))
+print(l)
 
 
 def guess_engine():
     guess = int(input("Please enter a number between 1 and 100: "))
     if guess < n:
         print(f"Please guess a higher number than {guess}.")
-        del numbers_list[:guess]
-        print(numbers_list)
         guess_engine()
     elif guess > n:
         print(f"Please guess a lower number than {guess}.")
-        del numbers_list[guess - 1:]
-        print(numbers_list)
         guess_engine()
     else:
         print("You guessed the correct number!")
