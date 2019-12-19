82fb82a9aad1360614ccc1828dc61690177b8508
EricMTodd <eterictodd@gmail.com>
2019-12-18 23:21:08 -0700

    small bugs with list being erased entirely after third guess, or numbers not being removed correctly at all.

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,6 +1,8 @@
+# import os
 from random import randint
+
 n = randint(1, 100)
-print(n)
+# print(n)
 numbers_list = []
 
 for i in range(1, 101):
@@ -11,16 +13,19 @@ print(numbers_list)
 def guess_engine():
     guess = int(input("Please enter a number between 1 and 100: "))
     if guess < n:
-        print("Please guess a higher number.")
+        # os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"Please guess a higher number than {guess}.")
         del numbers_list[:guess]
         print(numbers_list)
         guess_engine()
     elif guess > n:
-        print("Please guess a lower number.")
+        # os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"Please guess a lower number than {guess}.")
         del numbers_list[guess - 1:]
         print(numbers_list)
         guess_engine()
     else:
+        # os.system('cls' if os.name == 'nt' else 'clear')
         print("You guessed the correct number!")
 
 
