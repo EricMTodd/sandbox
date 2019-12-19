745ff63fb9908fa259b922ab9c03e81e411aec1e
EricMTodd <eterictodd@gmail.com>
2019-12-19 15:29:42 -0700

    solution in progress

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -7,14 +7,8 @@ print(l)
 
 def guess_engine():
     guess = int(input("Please enter a number between 1 and 100: "))
-    if guess < n:
-        print(f"Please guess a higher number than {guess}.")
-        guess_engine()
-    elif guess > n:
-        print(f"Please guess a lower number than {guess}.")
-        guess_engine()
-    else:
-        print("You guessed the correct number!")
+    index = l.index(guess)
+    print(index)
 
 
 guess_engine()
