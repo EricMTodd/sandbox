64aaea92822e9936874b23ca241def53cd3b0bc0
EricMTodd <eterictodd@gmail.com>
2019-12-18 23:05:58 -0700

    improved upon first version by adding a list that gets updated when numbers are guessed incorrectly.

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,15 +1,24 @@
-from random import randrange
-n = randrange(1, 100)
+from random import randint
+n = randint(1, 100)
 print(n)
+numbers_list = []
+
+for i in range(1, 101):
+    numbers_list.append(i)
+print(numbers_list)
 
 
 def guess_engine():
     guess = int(input("Please enter a number between 1 and 100: "))
     if guess < n:
         print("Please guess a higher number.")
+        del numbers_list[:guess]
+        print(numbers_list)
         guess_engine()
     elif guess > n:
         print("Please guess a lower number.")
+        del numbers_list[guess - 1:]
+        print(numbers_list)
         guess_engine()
     else:
         print("You guessed the correct number!")
