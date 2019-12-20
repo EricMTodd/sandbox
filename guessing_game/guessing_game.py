b1011823c2c51512884df4acc2c6bf634a00152c
EricMTodd <eterictodd@gmail.com>
2019-12-19 18:59:25 -0700

    bugs resolved with the help of u/Vaphell and u/SoNotRedditingAtWork from the r/learnpython subreddit.

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -2,13 +2,27 @@ from random import randint
 
 n = randint(1, 100)
 l = list(range(1, 101))
-print(l)
 
 
-def guess_engine():
-    guess = int(input("Please enter a number between 1 and 100: "))
-    index = l.index(guess)
-    print(index)
+def engine(number, choices):
+    while True:
+        print(choices)
+        try:
+            guess = int(
+                input(f"Please enter a number from {min(choices)} to {max(choices)}: "))
+            if not min(choices) <= guess <= max(choices):
+                raise ValueError
+            if guess == number:
+                return "You guessed the correct number!"
+            if guess < number:
+                print(f"Please guess a higher number than {guess}.")
+                choices = [x for x in choices if x > guess]
+            else:
+                print(f"Please guess a number lower than {guess}.")
+                choices = [x for x in choices if x < guess]
+        except ValueError:
+            print(
+                f"Invalid input! Your guess must be a number from {min(choices)} to {max(choices)}")
 
 
-guess_engine()
+print(engine(n, l))
