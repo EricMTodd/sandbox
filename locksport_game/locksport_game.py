1f49a3c8460e8b51b2ea8739ec6746f23ac5e90c
EricMTodd <eterictodd@gmail.com>
2020-01-26 14:44:38 -0700

    added limited number of attempts using a multiplier based on the number of tumblers in the lock.

--- locksport_game/locksport_game.py
+++ locksport_game/locksport_game.py
@@ -1,13 +1,17 @@
 from random import randint
+from math import ceil
 
 
-def printIntroduction(difficulty):
+def printIntroduction(maxLevel, attempts):
     print("\nYou've just stolen a pricelss artifact worth a fortune, it won't be long until you're caught.")
 
     print("You find a locked door barring your escape. You pull out your lockpicking kit and get to work.\n")
 
+    print(
+        f"This lock has {maxLevel} tumblers and you have {attempts} lockpicks you can use, try not to break them!\n")
 
-def playGame(difficulty):
+
+def playGame(difficulty, maxLevel, attempts):
 
     codeA = int(randint(1, 2 + difficulty))
     codeB = int(randint(1, 2 + difficulty))
@@ -16,12 +20,13 @@ def playGame(difficulty):
     codeSum = int(codeA) + int(codeB) + int(codeC)
     codeProduct = int(codeA) * int(codeB) * int(codeC)
 
-    if difficulty < 5:
-        print(
-            f"You are currently on tumbler number {difficulty}.\n{6 - difficulty} tumblers left.\n")
-    else:
+    if difficulty == 5:
         print(
             f"You're on the last tumbler!\n")
+
+    else:
+        print(
+            f"You are currently on tumbler number {difficulty}.\n{maxLevel - difficulty} tumblers left.\n")
 
     print("You must guess a three number code in order to successfully fit each tumbler into place.")
     print(f"The sum of the numbers in the code is {codeSum}.")
@@ -32,18 +37,14 @@ def playGame(difficulty):
         guessB = int(input("Please guess the second digit of the code: "))
         guessC = int(input("Please guess the third digit of the code: "))
 
-        print(f"Your guess: {guessA} {guessB} {guessC}")
+        print(f"Your guess: {guessA} {guessB} {guessC}\n")
 
         guessSum = guessA + guessB + guessC
         guessProduct = guessA * guessB * guessC
 
         if (guessSum == codeSum and guessProduct == codeProduct):
-            print(
-                f"Success!\nYou feel the tumbler slide into place.\n")
             return True
         else:
-            print(
-                "FAIL.\nThe guards have caught you, they beat you into unconsciousness...\n")
             return False
     except ValueError:
         print("\n***INVALID INPUT: You have entered an invalid character. Please try again.***\n")
@@ -52,16 +53,32 @@ def playGame(difficulty):
 def main():
     levelDifficulty = int(1)
     maxLevel = int(5)
-    printIntroduction(levelDifficulty)
+    attempts = int(ceil(maxLevel * 0.25))
+
+    printIntroduction(maxLevel, attempts)
 
     while (levelDifficulty <= maxLevel):
-        levelComplete = playGame(levelDifficulty)
+        levelComplete = playGame(levelDifficulty, maxLevel, attempts)
 
         if (levelComplete == True):
             levelDifficulty += 1
+            print(
+                f"SUCCESS!\nYou feel the tumbler slide into place.\n")
+        else:
+            attempts -= 1
+            if attempts == 1:
+                print(
+                    f"FAIL.\nYou have broken one of your lock picks. You have {attempts} lockpick left.\n")
+            else:
+                if attempts == 0:
+                    print(
+                        "You have broken all of you lockpicks, you have no way to escape.\nGAME OVER.\n")
+                    return 0
+                print(
+                    f"FAIL.\nYou have broken one of your lock picks. You have {attempts} lockpicks left.\n")
 
     print(
-        "You pop the lock, and throw open the door at a dead sprint into the night!")
+        "You pop the lock, and throw open the door at a dead sprint into the night!\n")
     return 0
 
 
